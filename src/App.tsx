cbe02468c454335a8d860ef177052fbe2f824087
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-09-23 09:34:41 +1000

    [dyad] Fixed React.Children.only error by adjusting TooltipProvider's children - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -28,30 +28,28 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <BrowserRouter>
-        <SessionContextProvider> {/* SessionContextProvider is now inside BrowserRouter */}
-          <TooltipProvider>
-            <React.Fragment>
-              <Sonner />
-              <Routes>
-                <Route path="/" element={<Layout><Index /></Layout>} />
-                <Route path="/resources" element={<Layout><Resources /></Layout>} />
-                <Route path="/events" element={<Layout><Events /></Layout>} />
-                <Route path="/login" element={<Layout><Login /></Layout>} />
-                <Route path="/current-event" element={<Layout><CurrentEventPage /></Layout>} />
-                <Route path="/learn-more" element={<Layout><LearnMore /></Layout>} />
-                
-                <Route path="/profile" element={<Layout><ProfileLayoutPage /></Layout>}>
-                  <Route index element={<ProfileDetails />} />
-                  <Route path="survey" element={<SurveyPage />} />
-                </Route>
+        <SessionContextProvider>
+          <Sonner /> {/* Moved Sonner here, outside TooltipProvider */}
+          <TooltipProvider> {/* Now directly wraps only Routes */}
+            <Routes>
+              <Route path="/" element={<Layout><Index /></Layout>} />
+              <Route path="/resources" element={<Layout><Resources /></Layout>} />
+              <Route path="/events" element={<Layout><Events /></Layout>} />
+              <Route path="/login" element={<Layout><Login /></Layout>} />
+              <Route path="/current-event" element={<Layout><CurrentEventPage /></Layout>} />
+              <Route path="/learn-more" element={<Layout><LearnMore /></Layout>} />
+              
+              <Route path="/profile" element={<Layout><ProfileLayoutPage /></Layout>}>
+                <Route index element={<ProfileDetails />} />
+                <Route path="survey" element={<SurveyPage />} />
+              </Route>
 
-                <Route path="/admin" element={<Layout><AdminZone /></Layout>} />
-                <Route path="/admin/members" element={<Layout><AdminMembers /></Layout>} />
-                <Route path="/admin/survey-data" element={<Layout><AdminSurveyData /></Layout>} />
-                <Route path="/admin/interest-submissions" element={<Layout><AdminInterestSubmissions /></Layout>} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </React.Fragment>
+              <Route path="/admin" element={<Layout><AdminZone /></Layout>} />
+              <Route path="/admin/members" element={<Layout><AdminMembers /></Layout>} />
+              <Route path="/admin/survey-data" element={<Layout><AdminSurveyData /></Layout>} />
+              <Route path="/admin/interest-submissions" element={<Layout><AdminInterestSubmissions /></Layout>} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
           </TooltipProvider>
         </SessionContextProvider>
       </BrowserRouter>
