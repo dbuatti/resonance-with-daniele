2aa2fb1ebbf5fbb27193fa6dc071637506e5094b
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-09-22 16:14:19 +1000

    [dyad] Moved Toaster and Sonner to main.tsx and fixed route typo. - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -14,27 +12,22 @@ import React from "react"; // Import React for Fragment
 const queryClient = new QueryClient();
 
 const App = () => (
-  <React.Fragment> {/* This Fragment is the single element returned by the App component */}
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider> {/* TooltipProvider is the single child of QueryClientProvider */}
-        <BrowserRouter> {/* BrowserRouter is the single child of TooltipProvider */}
-          <SessionContextProvider>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="/resources" element={<Resources />} />
-              <Route path="/events" element={<Events />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </SessionContextProvider>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
-    {/* Toaster and Sonner are now siblings to the entire provider chain, at the top level */}
-    <Toaster />
-    <Sonner />
-  </React.Fragment>
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <BrowserRouter>
+        <SessionContextProvider>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="/resources" element={<Resources />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </SessionContextProvider>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
 );
 
 export default App;
